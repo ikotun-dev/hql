9bcd32abd34461c8a86fc58b355dbb0a1299ee39
ikotun-dev <danlogan2003@gmail.com>
2024-01-01 19:57:42 +0100

    initial commie

---
+++ src/dataset.ts
@@ -0,0 +1,15 @@
+let people: {
+    id: number;
+    name: string;
+  }[] = [
+    {
+      id: 4,
+      name: "mide",
+    },
+    {
+      id : 5,
+      name: "John"
+    }
+  ];
+  
+export default people;
