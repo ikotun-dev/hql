ac14fad324bf90b0bc2f02e0e22af773ffbbf551
ikotun-dev <danlogan2003@gmail.com>
2024-01-01 23:03:18 +0100

    add user to db

--- src/Resolvers.ts
+++ src/Resolvers.ts
@@ -1,13 +1,13 @@
-//get all of the available data from our database.
+
 import UserModel from "./models/user";
 
 const Resolvers = {
   Query: {
-    getAllUsers: async () => await UserModel.find(), //if the user runs the getAllPeople command
-    //if the user runs the getPerson command:
+    getAllUsers: async () => await UserModel.find(), 
+    
     getUser: (_: any, args: any) => { 
       console.log(args);
-      //get the object that contains the specified ID.
+    
       return UserModel.findById(args.id);
     },
   },
